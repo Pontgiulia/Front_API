990a8cc4465144cf96f27f68cdab09b620af5e16
Pontgiulia <pontelligiulia@gmail.com>
2025-09-22 19:29:55 -0300

    front

--- frontApi/front-api/src/pages/Home/index.jsx
+++ frontApi/front-api/src/pages/Home/index.jsx
@@ -1,16 +1,23 @@
+import {useEffect,useState} from 'react'
 import './style.css'
 import Lixeira from "../../assets/lixeira.svg"
+import api from "../../services/api"
 
 function Home() {
-  const usuarios =[{
-    id:1,
-    nome:"Giulia",
-    email:"@email.com",
-    idade:18
-  }]
+  const [usuarios,setUsuarios] = useState([])
+
+  async function getUsuarios(){
+    const usuariosApi = await api.get('/cadastro')
+    setUsuarios(usuariosApi.data)
+    console.log(usuarios)
+  }
+  useEffect(()=>{
+    getUsuarios()
+  },[])
+
   return(
-    <div className="formu">
-      <form id>
+    <div className="container">
+      <form>
         <h1>Cadastro de Usuários</h1>
         <input name="nome" type="text"></input>
         <input name="email" type="email"></input>
@@ -20,7 +27,7 @@ function Home() {
       {usuarios.map(usuario =>(
         <div key={usuario.id} className='card'>
           <div>
-            <p>Nome: {usuario.name}</p>
+            <p>Nome: {usuario.nome}</p>
             <p>Email: {usuario.email}</p>
             <p>Idade: {usuario.idade}</p>
           </div>
